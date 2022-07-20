1fab6611308a3bbbb63191f00a24f07024fa41bb
Sara01 <saraosses01@gmail.com>
2022-07-19 20:26:37 -0400

    Añadido cooldown a disparador y ajustado movimiento de nave

--- Assets/Scripts/Disparador.cs
+++ Assets/Scripts/Disparador.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class Disparador : MonoBehaviour {
     public GameObject _prefabBala;
+    public float _enfriamientoDisparo = 0.3f;
+    
+    private float _temporizador;
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) 
-            Instantiate(_prefabBala, transform.position, transform.rotation);
+
+        _temporizador -= Time.deltaTime;
+
+        if (_temporizador <= 0f) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                
+                Instantiate(_prefabBala, transform.position, transform.rotation);
+                _temporizador = _enfriamientoDisparo;
+            }
+        }
     }
 }
